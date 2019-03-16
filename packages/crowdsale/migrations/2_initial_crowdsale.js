c8fe590b0ddfc0cb74323e03771fde5fad85328b
Fabian Raetz <fabian.raetz@gmail.com>
2019-03-16 14:10:32 +0100

    cleanup migration script

--- packages/crowdsale/migrations/2_initial_crowdsale.js
+++ packages/crowdsale/migrations/2_initial_crowdsale.js
@@ -1,92 +1,83 @@
 const moment = require('moment');
 
-const AsureCrowdsaleDeployer = artifacts.require("./AsureCrowdsaleDeployer.sol");
+const AsureCrowdsaleDeployer = artifacts.require("AsureCrowdsaleDeployer");
 const TokenVesting = artifacts.require("TokenVesting");
 
 
+async function createVestingContract(beneficiary, mainSaleOpeningTime, twoyears) {
+    const instance = await TokenVesting.new(
+        beneficiary.addr, //address
+        mainSaleOpeningTime.unix(), //unix timestemp
+        0, //cliffDuration
+        twoyears, // duration in sec ~2 yr = 60*60*24*365*2
+        false // bool revocable
+    );
+    beneficiary.tokenVestingAddr = instance.address;
+}
+
 module.exports = async function (deployer, network) {
-  let owner = '0x38D1Ea5E7EA932E83b482F4816F2ee1C61A288c2';
-  if (network === 'development') {
-    owner = '0xd7da996cc3c3186b87c5ea23599dec97153bcc21'; // account 4
-  }
-  const twoyears = 63072000; // ~2 yr = 60*60*24*365*2
-  const dateFormat = "YYYY-MM-DD HH:mm";
-  const preSaleOpeningTime = moment('2019-08-01 00:00', dateFormat);
-  const preSaleClosingTime = moment('2019-08-15 00:00', dateFormat);
+    await deployer;
 
-  const mainSaleOpeningTime = moment('2019-12-1 00:00', dateFormat);
-  const mainSaleClosingTime = moment('2019-12-31 00:00', dateFormat);
-
-  //Team
-  const teamBeneficiaries = [
-    {
-      _addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-      _amount: 1,
-      _tokenVestingAddr: null
-    },
-    {
-      _addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-      _amount: 1,
-      _tokenVestingAddr: null
+    let owner = '0x38D1Ea5E7EA932E83b482F4816F2ee1C61A288c2';
+    if (network === 'development') {
+        owner = '0xd7da996cc3c3186b87c5ea23599dec97153bcc21'; // account 4
     }
-  ];
 
-  for (let i = 0; i < teamBeneficiaries.length; i++) {
-    const instance = await deployer.deploy(
-      TokenVesting,
-      teamBeneficiaries[i]._addr, //address
-      mainSaleOpeningTime.unix(), //unix timestemp
-      0, //cliffDuration
-      twoyears, // duration in sec ~2 yr = 60*60*24*365*2
-      false // bool revocable
-    );
-    //console.log("instance: ", instance);
-    teamBeneficiaries[i]._tokenVestingAddr = TokenVesting.address;
-    console.log("teamBeneficiaries[i]._tokenVestingAddr:" + teamBeneficiaries[i]._tokenVestingAddr);
-  }
+    const twoyears = 63072000; // ~2 yr = 60*60*24*365*2
+    const dateFormat = "YYYY-MM-DD HH:mm";
+    const preSaleOpeningTime = moment('2019-08-01 00:00', dateFormat);
+    const preSaleClosingTime = moment('2019-08-15 00:00', dateFormat);
 
-  //Advisor
-  const advisorBeneficiaries = [
-    {
-      _addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-      _amount: 1,
-      _tokenVestingAddr: null
-    },
-    {
-      _addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-      _amount: 1,
-      _tokenVestingAddr: null
+    const mainSaleOpeningTime = moment('2019-12-1 00:00', dateFormat);
+    const mainSaleClosingTime = moment('2019-12-31 00:00', dateFormat);
+
+    const teamBeneficiaries = [
+        {
+            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
+            amount: 1,
+            tokenVestingAddr: null
+        },
+        {
+            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
+            amount: 1,
+            tokenVestingAddr: null
+        }
+    ];
+
+    const advisorBeneficiaries = [
+        {
+            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
+            amount: 1,
+            tokenVestingAddr: null
+        },
+        {
+            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
+            amount: 1,
+            tokenVestingAddr: null
+        }
+    ];
+
+    for (let i = 0; i < teamBeneficiaries.length; i++) {
+        await createVestingContract(teamBeneficiaries[i], mainSaleOpeningTime, twoyears);
     }
-  ];
 
-  for (let i = 0; i < advisorBeneficiaries.length; i++) {
-    const instance = await deployer.deploy(
-      TokenVesting,
-      advisorBeneficiaries[i]._addr, //address
-      mainSaleOpeningTime.unix(), //unix timestemp
-      0, //cliffDuration
-      twoyears, // duration in sec ~2 yr = 60*60*24*365*2
-      false // bool revocable
+    for (let i = 0; i < advisorBeneficiaries.length; i++) {
+        await createVestingContract(advisorBeneficiaries[i], mainSaleOpeningTime, twoyears);
+    }
+
+    const crowdsale = await AsureCrowdsaleDeployer.new(
+        owner,
+        preSaleOpeningTime.unix(),
+        preSaleClosingTime.unix(),
+        mainSaleOpeningTime.unix(),
+        mainSaleClosingTime.unix(),
+        teamBeneficiaries.map(b => b.tokenVestingAddr),
+        teamBeneficiaries.map(b => b.amount),
+        advisorBeneficiaries.map(b => b.tokenVestingAddr),
+        advisorBeneficiaries.map(b => b.amount),
     );
 
-    //console.log("instance: ", instance);
-    advisorBeneficiaries[i]._tokenVestingAddr = TokenVesting.address;
-    console.log("advisorBeneficiaries[i]._tokenVestingAddr:" + advisorBeneficiaries[i]._tokenVestingAddr);
-  }
-
-  //AsureCrowdsale
-  await deployer.deploy(
-    AsureCrowdsaleDeployer,
-    owner,
-    preSaleOpeningTime.unix(),
-    preSaleClosingTime.unix(),
-    mainSaleOpeningTime.unix(),
-    mainSaleClosingTime.unix(),
-    teamBeneficiaries.map(b => b._tokenVestingAddr),
-    teamBeneficiaries.map(b => b._amount),
-    advisorBeneficiaries.map(b => b._tokenVestingAddr),
-    advisorBeneficiaries.map(b => b._amount),
-  );
+    console.log(`AsureCrowdsale deployed at ${crowdsale.address}`);
 };
 
 /*
