66518caf9070fa4619d90c86f8bfdaed7e87378a
Fabian Raetz <fabian.raetz@gmail.com>
2019-03-16 15:34:58 +0100

    add support for config file per network

--- packages/crowdsale/migrations/2_initial_crowdsale.js
+++ packages/crowdsale/migrations/2_initial_crowdsale.js
@@ -1,29 +1,14 @@
 const fs = require('fs');
+const path = require('path');
 const moment = require('moment');
-const crowdsale = require('../crowdsale.json');
 
 const AsureCrowdsaleDeployer = artifacts.require("AsureCrowdsaleDeployer");
 const TokenVesting = artifacts.require("TokenVesting");
 
-
-async function createVestingContract(beneficiary, mainSaleOpeningTime, twoyears) {
-    const instance = await TokenVesting.new(
-        beneficiary.owner, //address
-        mainSaleOpeningTime, //unix timestemp
-        0, //cliffDuration
-        twoyears, // duration in sec ~2 yr = 60*60*24*365*2
-        false // bool revocable
-    );
-    beneficiary.addr = instance.address;
-}
-
 module.exports = async function (deployer, network) {
     await deployer;
 
-    let owner = '0x38D1Ea5E7EA932E83b482F4816F2ee1C61A288c2';
-    if (network === 'development') {
-        owner = '0xd7da996cc3c3186b87c5ea23599dec97153bcc21'; // account 4
-    }
+    const crowdsale = loadCrowdsaleConfig(network);
 
     const preSaleOpeningTime = moment(crowdsale.preSale.opening, crowdsale.dateFormat).unix();
     const preSaleClosingTime = moment(crowdsale.preSale.closing, crowdsale.dateFormat).unix();
@@ -41,7 +26,7 @@ module.exports = async function (deployer, network) {
     }
 
     const crowdsaleDeployer = await AsureCrowdsaleDeployer.new(
-        owner,
+        crowdsale.owner,
         preSaleOpeningTime,
         preSaleClosingTime,
         mainSaleOpeningTime,
@@ -56,12 +41,29 @@ module.exports = async function (deployer, network) {
     crowdsale.mainSale.addr = await crowdsaleDeployer.mainsale.call();
 
     console.log(`AsureCrowdsale deployment successful.`);
-    fs.writeFileSync(`crowdsale-${network}.json`, JSON.stringify(crowdsale, null, 2));
+    fs.writeFileSync(`crowdsale-${network}-final.json`, JSON.stringify(crowdsale, null, 2));
 };
 
-/*
-const oneyear = 31536000 // ~1 yr = 60*60*24*365
-const twoyears = 63072000 // ~2 yr = 60*60*24*365*2
-const threeyears = 94608000 // ~3 yr = 60*60*24*365*3
-const fouryears = 126144000 // ~4yrs =60*60*24*365*4
-*/
+
+async function createVestingContract(beneficiary, mainSaleOpeningTime, twoyears) {
+    const instance = await TokenVesting.new(
+        beneficiary.owner, //address
+        mainSaleOpeningTime, //unix timestemp
+        0, //cliffDuration
+        twoyears, // duration in sec ~2 yr = 60*60*24*365*2
+        false // bool revocable
+    );
+    beneficiary.addr = instance.address;
+}
+
+function loadCrowdsaleConfig(network) {
+    const fileName = `crowdsale-${network}.json`;
+
+    if (!fs.existsSync(path.join(process.cwd(), fileName))) {
+        throw Error(
+            `No crowdsale configuration file (${path.join(process.cwd(), fileName)}) for network "${network}" found.`
+        )
+    }
+
+    return require(`../${fileName}`);
+}
