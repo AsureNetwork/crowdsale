9d02d500e2f41bbc3d7649891c327022ffb877a3
Fabian Raetz <fabian.raetz@gmail.com>
2019-03-16 14:55:52 +0100

    move all tge related values into a seperate json file

--- packages/crowdsale/migrations/2_initial_crowdsale.js
+++ packages/crowdsale/migrations/2_initial_crowdsale.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const moment = require('moment');
+const crowdsale = require('../crowdsale.json');
 
 const AsureCrowdsaleDeployer = artifacts.require("AsureCrowdsaleDeployer");
 const TokenVesting = artifacts.require("TokenVesting");
@@ -6,13 +8,13 @@ const TokenVesting = artifacts.require("TokenVesting");
 
 async function createVestingContract(beneficiary, mainSaleOpeningTime, twoyears) {
     const instance = await TokenVesting.new(
-        beneficiary.addr, //address
-        mainSaleOpeningTime.unix(), //unix timestemp
+        beneficiary.owner, //address
+        mainSaleOpeningTime, //unix timestemp
         0, //cliffDuration
         twoyears, // duration in sec ~2 yr = 60*60*24*365*2
         false // bool revocable
     );
-    beneficiary.tokenVestingAddr = instance.address;
+    beneficiary.addr = instance.address;
 }
 
 module.exports = async function (deployer, network) {
@@ -23,61 +25,38 @@ module.exports = async function (deployer, network) {
         owner = '0xd7da996cc3c3186b87c5ea23599dec97153bcc21'; // account 4
     }
 
+    const preSaleOpeningTime = moment(crowdsale.preSale.opening, crowdsale.dateFormat).unix();
+    const preSaleClosingTime = moment(crowdsale.preSale.closing, crowdsale.dateFormat).unix();
+    const mainSaleOpeningTime = moment(crowdsale.mainSale.opening, crowdsale.dateFormat).unix();
+    const mainSaleClosingTime = moment(crowdsale.mainSale.closing, crowdsale.dateFormat).unix();
+
     const twoyears = 63072000; // ~2 yr = 60*60*24*365*2
-    const dateFormat = "YYYY-MM-DD HH:mm";
-    const preSaleOpeningTime = moment('2019-08-01 00:00', dateFormat);
-    const preSaleClosingTime = moment('2019-08-15 00:00', dateFormat);
 
-    const mainSaleOpeningTime = moment('2019-12-1 00:00', dateFormat);
-    const mainSaleClosingTime = moment('2019-12-31 00:00', dateFormat);
-
-    const teamBeneficiaries = [
-        {
-            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-            amount: 1,
-            tokenVestingAddr: null
-        },
-        {
-            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-            amount: 1,
-            tokenVestingAddr: null
-        }
-    ];
-
-    const advisorBeneficiaries = [
-        {
-            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-            amount: 1,
-            tokenVestingAddr: null
-        },
-        {
-            addr: '0xcbBc3D3d381f3A9a48CbAE9Ca701aC3c92e0aEA5',
-            amount: 1,
-            tokenVestingAddr: null
-        }
-    ];
-
-    for (let i = 0; i < teamBeneficiaries.length; i++) {
-        await createVestingContract(teamBeneficiaries[i], mainSaleOpeningTime, twoyears);
+    for (let i = 0; i < crowdsale.team.length; i++) {
+        await createVestingContract(crowdsale.team[i], mainSaleOpeningTime, twoyears);
     }
 
-    for (let i = 0; i < advisorBeneficiaries.length; i++) {
-        await createVestingContract(advisorBeneficiaries[i], mainSaleOpeningTime, twoyears);
+    for (let i = 0; i < crowdsale.advisor.length; i++) {
+        await createVestingContract(crowdsale.advisor[i], mainSaleOpeningTime, twoyears);
     }
 
-    const crowdsale = await AsureCrowdsaleDeployer.new(
+    const crowdsaleDeployer = await AsureCrowdsaleDeployer.new(
         owner,
-        preSaleOpeningTime.unix(),
-        preSaleClosingTime.unix(),
-        mainSaleOpeningTime.unix(),
-        mainSaleClosingTime.unix(),
-        teamBeneficiaries.map(b => b.tokenVestingAddr),
-        teamBeneficiaries.map(b => b.amount),
-        advisorBeneficiaries.map(b => b.tokenVestingAddr),
-        advisorBeneficiaries.map(b => b.amount),
+        preSaleOpeningTime,
+        preSaleClosingTime,
+        mainSaleOpeningTime,
+        mainSaleClosingTime,
+        crowdsale.team.map(b => b.addr),
+        crowdsale.team.map(b => b.amount),
+        crowdsale.advisor.map(b => b.addr),
+        crowdsale.advisor.map(b => b.amount),
     );
+    crowdsale.token.addr = await crowdsaleDeployer.token.call();
+    crowdsale.preSale.addr = await crowdsaleDeployer.presale.call();
+    crowdsale.mainSale.addr = await crowdsaleDeployer.mainsale.call();
 
-    console.log(`AsureCrowdsale deployed at ${crowdsale.address}`);
+    console.log(`AsureCrowdsale deployment successful.`);
+    fs.writeFileSync(`crowdsale-${network}.json`, JSON.stringify(crowdsale, null, 2));
 };
 
 /*
