460178e46f2d54df470628ab501f22cbe1e7257a
Fabian Raetz <fabian.raetz@gmail.com>
2019-03-21 19:20:02 +0100

    fix code coverage?

--- packages/crowdsale/.solcover.js
+++ packages/crowdsale/.solcover.js
@@ -1,9 +1,10 @@
 module.exports = {
   port: 8555,
   testrpcOptions: '-p 8555 --mnemonic "flight cute ski coffee decide milk bitter table speed orchard bag meadow"',
-  testCommand: 'mocha --timeout 5000',
-  norpc: true,
+  //norpc: true,
   dir: '.',
   copyPackages: ['openzeppelin-solidity'],
-  skipFiles: ['test/TestToken.sol','test/TestAsureBonusesCrowdsale.sol']
+  skipFiles: ['test/TestToken.sol','test/TestAsureBonusesCrowdsale.sol'],
+  compileCommand: '../node_modules/.bin/truffle compile',
+  testCommand: '../node_modules/.bin/truffle test --network coverage',
 };
--- packages/crowdsale/truffle-config.js
+++ packages/crowdsale/truffle-config.js
@@ -61,10 +61,10 @@ module.exports = {
       //skipDryRun: true
     },
     coverage: {
-      host: "127.0.0.1",
+      host: "localhost",
       network_id: "*",
       port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      //gas: 0xfffffffffff, // <-- Use this high gas value
+      gas: 0xfffffffffff, // <-- Use this high gas value
       gasPrice: 0x01      // <-- Use this low gas price
     },
     // Another network with more advanced options...
